9eb80e908411f1b63ba7dd06e92ec1b06068103c
HaoduyouduDev <michaelzhtiger@outlook.com>
2026-06-12 22:06:29 +0800

    Initial Commit

---
+++ Pitchee/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pitchee
+//
+//  Created by Lvy Zhan on 2026/6/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, github!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pitchee/PitcheeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitcheeApp.swift
+//  Pitchee
+//
+//  Created by Lvy Zhan on 2026/6/12.
+//
+
+import SwiftUI
+
+@main
+struct PitcheeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
